e326cca960e0a2c460e4ea5702cab543fe626c56
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-03-31 16:46:01 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿double milhas, km;
+Console.Write("Digite a medida em milhas: ");
+milhas = double.Parse(Console.ReadLine()!);
+km = 1.609 * milhas;
+
+Console.WriteLine($"A sua medida em km é {km}");
